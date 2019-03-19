a16e3482739bf7687ae7dfa2727601ce4182c6cf
allVentures <paul@allphotobangkok.com>
2019-03-19 11:10:18 +0700

    static files and main template

--- python_test/views.py
+++ python_test/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class MainPage(View):
+    def get(self, request):
+
+        ctx = {}
+        return render(request, "main_page.html", ctx)
